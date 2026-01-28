6732969208ba6d616eace5886bb8113d16b6c4b7
David Amigo <davamigo@gmail.com>
2026-01-28 17:13:04 +0100

    Rename projects 01 to 06 + New project 06-Arrays

--- 08-Bucles-for/Bucles-for/Program.cs
+++ 08-Bucles-for/Bucles-for/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Bucle while
 
+using System.Data.Common;
+
 Console.WriteLine("Ejemplo while");
 int num = 1;
 while (num <= 10)
@@ -24,3 +26,36 @@ Sintaxis for:
         // Instrucciones;
     }
 */
+
+// Ejemplo con break;
+/*
+while (true)
+{
+    Console.Write("Dime un número (0 para terminar): ");
+    int n = Convert.ToInt32(Console.ReadLine());
+
+    if (n == 0)
+    {
+        break;
+    }
+
+    if (n < 0)
+    {
+        Console.WriteLine(n + " es negativo");
+    }
+    else
+    {
+        Console.WriteLine(n + " es positivo");
+    }
+}
+*/
+
+// Bucles anidados - un bucle dentro de otro
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = i; j < 5; j++)
+    {
+        Console.WriteLine(j);
+    }
+}
---
+++ 09-Arrays/Arrays/Program.cs
@@ -0,0 +1,49 @@
+﻿// Ejemplo array de enteros
+int[] enteros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+int entero = enteros[0];
+Console.WriteLine(entero); // Escribirá un 1
+
+
+// Otra forma de crear un array
+int[] enteros2 = new int[7]; // [0, 0, 0, 0, 0, 0, 0]
+Console.WriteLine(enteros2[0]);
+
+
+// Un string es un array de caracteres
+string holaMundo = "Hola Mundo";
+char c = holaMundo[5];
+Console.WriteLine(c); // Escribirá M
+
+// Podemos ver el valor numérico del caracter M
+byte b = (byte) c;
+Console.WriteLine(b); // Escribirá 77 porque el valor ascii de la letra M es 77
+
+
+// Ejemplo foreach con un string
+Console.WriteLine(holaMundo);
+foreach(char letra in holaMundo)
+{
+    Console.WriteLine(letra);
+}
+
+// Lo mismo, pero con un bucle for
+for (int i = 0; i < holaMundo.Length; i++)
+{
+    char letra = holaMundo[i];
+    Console.WriteLine(letra);
+}
+
+// Ejemplo foreach con un int[]
+foreach(int num in enteros)
+{
+    Console.WriteLine(num);
+}
+
+/*
+Syntaxis:
+
+    foreach (tipo nombreVar in arrayOString)
+    {
+        // nombreVar --> tiene cada valor del array
+    }
+*/

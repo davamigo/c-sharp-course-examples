dc190388123a0ff5ebd80093ff9bdfa4fb801e04
David Amigo <davamigo@gmail.com>
2026-02-12 17:20:44 +0100

    Updated 10-Funciones

---
+++ 10-Funciones/Funciones/Functions.cs
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// La clase Functions sirve para demostrar la sobrecarga de funciones
+//
+
+class Functions
+{
+    public static int Suma (int a, int b)
+    {
+        return a + b;
+    }
+
+    public static double Suma (double a, double b)
+    {
+        return a + b;
+    }
+
+    public static string Suma(string a, string b)
+    {
+        return a + b;
+    }
+}
--- 10-Funciones/Funciones/Program.cs
+++ 10-Funciones/Funciones/Program.cs
@@ -1,30 +1,34 @@
-﻿/*
-Una variable es una forma de darle un nombre a un valor
-
-Una función es una forma de darle nombre a un trozo de código... para usarlo después
-
-Sintaxis:
-
-    tipo-de-retorno nombre-función ( parametro1, paremetro2, ... )
-*/
-
+﻿///////////////////////////////////////////////////////////////////////////////////////////////////
+// Una variable es una forma de darle un nombre a un valor
+//
+// Una función es una forma de darle nombre a un "trozo de código"... para usarlo después
+//
+// Sintaxis:
+//   tipo-de-retorno nombre-función ( tipo1 parámetro1, tipo2 parámetro2, ... )
+//
 // Ejemplo: Función Hola Mundo
 
+// Definición de la función
 void HolaMundo()
 {
     Console.WriteLine("Hola Mundo!");
 }
 
+// Llamada a la función
 HolaMundo();
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 // Ejemplo: Función Suma Dos Numeros
 
+// Definición de la función
 void SumaDosNumeros(int num1, int num2)
 {
     int suma = num1 + num2;
     Console.WriteLine(num1 + " + " + num2 + " = " + suma);
 }
 
+// Llamadas a la función
 SumaDosNumeros(1, 5);
 SumaDosNumeros(2, 7);
 SumaDosNumeros(-1, -5);
@@ -36,6 +40,8 @@ for (int i = 1; i <= 5; i++)
     SumaDosNumeros(i, i);
 }
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
 // Ejemplo: Función WL como atajo para Console.WriteLine
 
 void WL(string text)
@@ -45,6 +51,10 @@ void WL(string text)
 
 WL("Hola mundo");
 
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// El nombre de la función tienen que explicar lo que hace
+
 int SumaDosNumerosYDevuelveResultado(int num1, int num2)
 {
     int suma = num1 + num2;
@@ -53,3 +63,53 @@ int SumaDosNumerosYDevuelveResultado(int num1, int num2)
 
 int result = SumaDosNumerosYDevuelveResultado(6, 7);
 WL("6 + 7 = " + result);
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Funciones dentro de funciones
+
+int varGlobal = 0;
+void bisabuelo()
+{
+    int varBisabuelo = varGlobal + 1;
+    WL("Dentro de bisabuelo " + varBisabuelo);
+    abuelo();
+
+    void abuelo()
+    {
+        int varAbuelo = varBisabuelo + 1;
+        WL("Dentro de abuelo " + varAbuelo);
+        padre();
+
+        void padre()
+        {
+            int varPadre = varAbuelo + 1;
+            WL("Dentro de padre " + varPadre);
+            hijo();
+
+            void hijo()
+            {
+                int varHijo = varPadre + 1;
+                WL("Dentro de hijo " + varHijo);
+            }
+        }
+    }
+}
+
+bisabuelo();
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+// Sobrecarga de funciones
+//
+// Las funciones sobrecargadas tienen el mismo nombre pero distintos parámetros
+//
+// Ver la clase Functions (en otro fichero)
+//
+// El compilador decide a que función llamar dependiendo de los parámetros que se pasen
+//
+
+Console.WriteLine(Functions.Suma(5, 7));
+Console.WriteLine(Functions.Suma(5.4, 7.8));
+Console.WriteLine(Functions.Suma("xxxx", "yyyy"));
+
